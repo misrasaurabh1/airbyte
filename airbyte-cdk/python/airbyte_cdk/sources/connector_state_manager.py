60fa3c68068cc20798fa51053b0bfd3eaf3c352f
Saurabh Misra <misra.saurabh1@gmail.com>
2024-09-09 00:15:08 -0700

    Convert HashableStreamDescriptor to a namedtuple for improved hashing performance

--- airbyte-cdk/python/airbyte_cdk/sources/connector_state_manager.py
+++ airbyte-cdk/python/airbyte_cdk/sources/connector_state_manager.py
@@ -3,23 +3,16 @@
 #
 
 import copy
-from dataclasses import dataclass
-from typing import Any, List, Mapping, MutableMapping, Optional, Tuple, Union
+from typing import Any, List, Mapping, MutableMapping, Optional, Tuple, Union, NamedTuple
 
-from airbyte_cdk.models import AirbyteMessage, AirbyteStateBlob, AirbyteStateMessage, AirbyteStateType, AirbyteStreamState, StreamDescriptor
+from airbyte_cdk.models import AirbyteMessage, AirbyteStateBlob, AirbyteStateMessage, AirbyteStateType, \
+    AirbyteStreamState, StreamDescriptor
 from airbyte_cdk.models import Type as MessageType
 
 
-@dataclass(frozen=True)
-class HashableStreamDescriptor:
-    """
-    Helper class that overrides the existing StreamDescriptor class that is auto generated from the Airbyte Protocol and
-    freezes its fields so that it be used as a hash key. This is only marked public because we use it outside for unit tests.
-    """
-
+class HashableStreamDescriptor(NamedTuple):
     name: str
-    namespace: Optional[str] = None
-
+    namespace: Optional[str]
 
 class ConnectorStateManager:
     """
